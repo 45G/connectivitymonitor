5bb877bf028bffabacfb62ef17c1fa569b423959
Laura Cristina Ruse <laura.ruse@cs.pub.ro>
2017-06-28 23:17:48 +0300

    ConnectivityMonitor: Start AsyncTask sequentially. Cancel current AsyncTask when needed. Clear output. Choose between native executable, python script and shell script. A more general approach.

--- app/src/main/java/com/a45g/athena/connectivitymonitor/TestFragment.java
+++ app/src/main/java/com/a45g/athena/connectivitymonitor/TestFragment.java
@@ -1,6 +1,8 @@
 package com.a45g.athena.connectivitymonitor;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -23,6 +25,7 @@ public class TestFragment extends Fragment {
 
     private static final String LOG_TAG = TestFragment.class.getName();
 
+    private Button mChooseType = null;
     private TextView mScriptText = null;
     private EditText mScriptName = null;
     private TextView mDomainText = null;
@@ -35,7 +38,10 @@ public class TestFragment extends Fragment {
     private TextView mResultText = null;
 
 
-    private Button mStart = null;
+    private Button mStartTest = null;
+    private Button mStopTest = null;
+    private Button mClearOutput = null;
+
     private TextView mResult = null;
     private LinearLayout mLayout = null;
     private View mScrollView = null;
@@ -44,10 +50,24 @@ public class TestFragment extends Fragment {
     private int mMaxScrollPosition;
     private Runnable checkScrollRunnable = null;
 
+    private String[] methods;
+    private int methodIndex = 1;
+    private String[] methodName;
+
+    private ExecuteTask task = null;
+    private boolean cancel = false;
+    private int times = 0;
+    private String mCmd = null;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.test_fragment, container, false);
+
+        methods = new String[]{"", "/data/user/0/org.qpython.qpy/files/bin/qpython-android5.sh ", "sh "};
+        methodName = getResources().getStringArray(R.array.execute_array);
+
+        mChooseType = (Button) rootView.findViewById(R.id.chooseType);
 
         mScriptText = (TextView) rootView.findViewById(R.id.scriptText);
         mScriptName = (EditText) rootView.findViewById(R.id.scriptName);
@@ -63,29 +83,68 @@ public class TestFragment extends Fragment {
 
         mResultText = (TextView) rootView.findViewById(R.id.resultText);
 
+        mStartTest = (Button) rootView.findViewById(R.id.start);
+        mStopTest = (Button) rootView.findViewById(R.id.stop);
+        mClearOutput = (Button) rootView.findViewById(R.id.clear);
 
-        mStart = (Button) rootView.findViewById(R.id.start);
         mResult = (TextView) rootView.findViewById(R.id.testResult);
         mLayout = (LinearLayout) rootView.findViewById(R.id.testLayout);
         mScrollView = (ScrollView) rootView.findViewById(R.id.testScrollView);
 
-        mStart.setOnClickListener(new View.OnClickListener() {
+        mChooseType.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(LOG_TAG, "test2");
+                showListView();
+            }
+        });
 
-                String times = mTimesValue.getText().toString();
+        mStartTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "Starting text execution");
 
                 mResult.setText("");
 
-                for (int i = 0; i < Integer.valueOf(times); i++) {
-                    new ExecuteTask().execute(
-                            "/data/user/0/org.qpython.qpy/files/bin/qpython-android5.sh " +
-                                    mScriptName.getText() + " " + mDomainName.getText() + " " +
-                                    mPortValue.getText() + " && exit");
+                if (methodIndex == 1){
+                    mCmd = methods[methodIndex] +
+                            mScriptName.getText() + " " + mDomainName.getText() + " " +
+                            mPortValue.getText() + " && exit";
+                    Log.d(LOG_TAG, "CMD: "+mCmd);
                 }
+                else{
+                    mCmd = methods[methodIndex] +
+                            mScriptName.getText();
+                    Log.d(LOG_TAG, "CMD: "+mCmd);
+                }
+
+                times = Integer.valueOf(mTimesValue.getText().toString());
+
+                if (times > 0) {
+                    task = new ExecuteTask();
+                    task.execute(mCmd);
+                }
+
             }
         });
+
+        mStopTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "Canceling test execution manually");
+
+                task.cancel(false);
+            }
+        });
+
+        mClearOutput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "Deleting output");
+
+                mResult.setText("");
+            }
+        });
+
 
         mResult.addTextChangedListener(new TextWatcher() {
 
@@ -129,32 +188,68 @@ public class TestFragment extends Fragment {
 
     private class ExecuteTask extends AsyncTask<String, Integer, String> {
 
-        protected String doInBackground(String... name) {
+        protected String doInBackground(String... params) {
 
-            String output = sudoForResult(name[0]);
-            String[] lineTokens = output.split("\n");
-            String[] tokens = lineTokens[0].split(" ");
+            String output = "";
 
-            if (tokens.length >= 5)
-                return tokens[4];
-            else
-                return null;
+            String result = sudoForResult(params[0]);
+            String[] lineTokens = result.split("\n");
+            //String[] tokens = lineTokens[0].split(" ");
+            //if (tokens.length >= 5)
+            //    Log.d(LOG_TAG, tokens[4]+" ms");
+
+            if (methodIndex == 1){
+                output = lineTokens[0];
+            }
+            else{
+                output = result;
+            }
+
+            Log.d(LOG_TAG, output);
+
+            DatabaseOperations databaseOperations = new DatabaseOperations(getContext());
+            databaseOperations.openWrite();
+            databaseOperations.insertTestResult(getTime(), methodName[methodIndex], output);
+            databaseOperations.close();
+
+            times--;
+
+            return output;
         }
 
         protected void onPostExecute(String result) {
 
             if (result == null) return;
 
-            DatabaseOperations databaseOperations = new DatabaseOperations(getContext());
-            databaseOperations.openWrite();
-            databaseOperations.insertTestResult(getTime(), "RTT", result);
-            databaseOperations.close();
-
             CharSequence previousResults = mResult.getText();
-            mResult.setText(previousResults + "\n" + result + " ms");
-            Log.d(LOG_TAG, result);
+            mResult.setText(previousResults + result + "\n");
+
+            if (times > 0) {
+                task = new ExecuteTask();
+                task.execute(mCmd);
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            Log.d(LOG_TAG,"Test execution has been canceled manually");
         }
     }
 
-
+    private void showListView() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.execute)
+                .setItems(R.array.execute_array, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // The 'which' argument contains the index position
+                        // of the selected item
+                        Log.d(LOG_TAG, "Selected option "+which);
+                        methodIndex = which;
+                        mScriptText.setText(methodName[methodIndex]+":");
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.getListView().setSelection(1);
+        alertDialog.show();
+    }
 }
